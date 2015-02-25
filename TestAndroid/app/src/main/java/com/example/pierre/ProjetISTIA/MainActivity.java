53215447183c09678d485ef9883d4549a13fb418
imefGames <michel.pierre.35@gmail.com>
2015-02-25 10:45:02 +0100

    projet avec gradle

---
+++ TestAndroid/app/src/main/java/com/example/pierre/ProjetISTIA/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.pierre.ProjetISTIA;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    private Game game;
+    private int sWidth, sHeight;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        sWidth = size.x;
+        sHeight = size.y;
+
+        game = new Game(this, sWidth, sHeight);
+        game.setBackgroundColor(Color.TRANSPARENT);
+        setContentView(game);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        return super.onOptionsItemSelected(item);
+    }
+}
