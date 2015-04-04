76f91026beac05d00f54be7e83757ca3dfcb5791
imefGames <michel.pierre.35@gmail.com>
2015-04-04 14:35:48 +0200

    Implementation de driftingdroids

--- IA_RicochetRobots/src/istia/ei4/pm/ia/ricochet/MainIA.java
+++ IA_RicochetRobots/src/istia/ei4/pm/ia/ricochet/MainIA.java
@@ -1,39 +1,62 @@
 package istia.ei4.pm.ia.ricochet;
 
+import driftingdroids.model.Board;
+import driftingdroids.model.Solution;
+import driftingdroids.model.Solver;
 import istia.ei4.ProjetISTIA.FileReadWrite;
 import istia.ei4.ProjetISTIA.GridElement;
 import istia.ei4.ProjetISTIA.MapObjects;
-import istia.ei4.pm.ia.*;
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainIA {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
     
-    RRWorld world = null;
-    String text = null;
-    RRGameState baseState = new RRGameState(null, null);
-    FileReadWrite myFile = new FileReadWrite();
-    text = myFile.read("generatedMap_153.txt");
-    ArrayList<GridElement> elements = MapObjects.extractDataFromString(text);
-    world = RRGetMap.createWorld(elements, baseState);
-    
-    //world.show(baseState);
-    
-    RREndCondition endCondition = new RREndCondition();
-    world.precomputeGrid();
-    
-		RRSolver solver = new RRSolver(10, world, baseState, endCondition);
-		
-		GameSolution solution = solver.solve();
-    
-    if(solution == null){
-      System.out.println("No Solution!");
-    }else{
-      for(IGameMove m : solution.getMoves()){
-        System.out.println(m);
-      }
+        /*RRWorld world = null;
+        String text = null;
+        RRGameState baseState = new RRGameState(null, null);
+        FileReadWrite myFile = new FileReadWrite();
+        text = myFile.read("generatedMap_154.txt");
+        ArrayList<GridElement> elements = MapObjects.extractDataFromString(text);
+        world = RRGetMap.createWorld(elements, baseState);
+
+        //world.show(baseState);
+
+        RREndCondition endCondition = new RREndCondition();
+        world.precomputeGrid();
+
+                    RRSolver solver = new RRSolver(10, world, baseState, endCondition);
+
+                    GameSolution solution = solver.solve();
+
+        if(solution == null){
+          System.out.println("No Solution!");
+        }else{
+          for(IGameMove m : solution.getMoves()){
+            System.out.println(m);
+          }
+        }//*/
+        
+        FileReadWrite myFile = new FileReadWrite();
+        String text = myFile.read("generatedMap_154.txt");
+        ArrayList<GridElement> elements = MapObjects.extractDataFromString(text);
+        
+        Board b = RRGetMap.createWorld(elements);
+        System.out.println("**********");
+        System.out.println(b.toString());
+        System.out.println("**********");
+        
+        
+        Solver s = Solver.createInstance(b);
+        try{
+            List<Solution> sol = s.execute();
+            for(Solution solution : sol){
+                System.out.println(solution.toMovelistString());
+            }
+        }catch(InterruptedException e){
+            System.err.println(e.getMessage());
+        }//*/
     }
-	}
 }
